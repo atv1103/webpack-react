a2a8299a6b4ad8b86e69045737260b9092528f99
Tatiana Anuchina <1103atv@gmail.com>
2025-10-14 22:00:53 +1000

    fix global.d.ts

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,18 +1,24 @@
+declare module '*.module.scss' {
+  const classes: { [key: string]: string }
+  export default classes
+}
+
 declare module '*.scss' {
-  interface IClassNames {
-    [className: string]: string
-  }
-  const classNames: IClassNames;
-  export = classNames;
+  const classes: { [key: string]: string }
+  export default classes
 }
 
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
+
 declare module "*.svg" {
-    import React from "react";
-    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-    export default SVG;
+  import * as React from "react";
+  const SVG: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
 }
 
 declare const __IS_DEV__: boolean;
+
+
+
